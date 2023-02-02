8439d288983610c83d1e32768b1921b2d818aee9
Krishnavamsi <vamsik532@gmail.com>
2023-02-02 19:03:25 +0530

    updated imports

--- python/creational/abstract-factory/main.py
+++ python/creational/abstract-factory/main.py
@@ -1,14 +1,13 @@
 from factory.ResourceFactory import ResourceFactory
 from factory.AWSResourceFactory import AWSResourceFactory
-from instance import Instance
-import os
+from instance.Instance import Instance
 
 
 class Client:
     def __init__(self, factory: ResourceFactory):
         self.factory = factory
 
-    def get_instance(self, size) -> Instance.Instance:
+    def get_instance(self, size) -> Instance:
         instance = self.factory.create_instance()
         storage = self.factory.create_storage(size)
         instance.attach_storage(storage)
@@ -16,7 +15,6 @@ class Client:
 
 
 if __name__ == "__main__":
-    print("current working directory " + os.getcwd())
     client = Client(AWSResourceFactory())
     inst = client.get_instance(100)
     inst.start()
